c500637646308fed700fffd0dc8c76623b813e9d
K-Elb <kareemkimo711@hotmail.com>
2026-01-10 19:46:43 +0400

    FIrst commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Karim Elbehiri on 10/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
